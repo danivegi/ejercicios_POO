bf27995ce0a23e25e7c2a4922d1041ad2feca0fb
danivegi <danielvegaalcazar@cdpjosecabrera.es>
2025-02-10 09:41:08 +0100

    Ejercicio 2
    método equals

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -39,6 +39,34 @@ public class Persona {
         return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + edad;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Persona other = (Persona) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (edad != other.edad)
+            return false;
+        return true;
+    }
+
     
     
 
--- src/vista/Main.java
+++ src/vista/Main.java
@@ -9,7 +9,11 @@ public class Main {
         // Métdodo Persona con nombre y edad
         Persona p1 = new Persona("Daniel", 25);
         System.out.println(p1);
-        
+
+        //Comparar con método equals p1 y p2
+        Persona p2 = new Persona("Daniel", 25);
+        System.out.println(p2);
+        System.out.println("¿Son iguales las dos personas introducidas? " + p1.equals(p2));
     }
 
 }
